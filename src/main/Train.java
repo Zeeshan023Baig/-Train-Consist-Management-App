fdd620c5d4eb24cfa0f81fe738be04ad2b3013a5
Zeeshan023Baig <zeeshan.baig.1323gmail.com>
2026-04-25 16:22:14 +0530

    UC19

--- src/main/Train.java
+++ src/main/Train.java
@@ -1,33 +1,51 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Train {
 
-    // 🔍 Linear Search Method
-    public static boolean linearSearch(String[] arr, String key) {
-        for (int i = 0; i < arr.length; i++) {
+    // 🔍 Binary Search Method
+    public static boolean binarySearch(String[] arr, String key) {
 
-            // Compare using equals()
-            if (arr[i].equals(key)) {
-                return true; // found → stop early
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            int cmp = key.compareTo(arr[mid]);
+
+            if (cmp == 0) {
+                return true; // found
+            } else if (cmp < 0) {
+                high = mid - 1; // search left
+            } else {
+                low = mid + 1; // search right
             }
         }
+
         return false; // not found
     }
 
     public static void main(String[] args) {
 
         System.out.println("=========================================");
-        System.out.println(" UC18 - Linear Search for Bogie ID ");
+        System.out.println(" UC19 - Binary Search for Bogie ID ");
         System.out.println("=========================================\n");
 
-        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+        String[] bogieIds = {"BG309","BG101","BG550","BG205","BG412"};
+
+        // 🔥 Step 1: Sort (IMPORTANT precondition)
+        Arrays.sort(bogieIds);
+
+        System.out.println("Sorted Bogie IDs:");
+        System.out.println(Arrays.toString(bogieIds));
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Bogie ID to search: ");
-        String searchKey = sc.nextLine();
+        System.out.print("\nEnter Bogie ID to search: ");
+        String key = sc.nextLine();
 
-        boolean found = linearSearch(bogieIds, searchKey);
+        boolean found = binarySearch(bogieIds, key);
 
         if (found) {
             System.out.println("Bogie FOUND ");
