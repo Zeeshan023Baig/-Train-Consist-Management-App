4aac1994112da95b345b1aed3e3be6209d5d19e2
Zeeshan023Baig <zeeshan.baig.1323gmail.com>
2026-04-25 16:19:14 +0530

    UC18

--- src/main/Train.java
+++ src/main/Train.java
@@ -1,26 +1,40 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Train {
+
+    // 🔍 Linear Search Method
+    public static boolean linearSearch(String[] arr, String key) {
+        for (int i = 0; i < arr.length; i++) {
+
+            // Compare using equals()
+            if (arr[i].equals(key)) {
+                return true; // found → stop early
+            }
+        }
+        return false; // not found
+    }
 
     public static void main(String[] args) {
 
         System.out.println("=========================================");
-        System.out.println(" UC17 - Sort Bogie Names using Arrays.sort()");
+        System.out.println(" UC18 - Linear Search for Bogie ID ");
         System.out.println("=========================================\n");
 
-        String[] bogieNames = {
-                "Sleeper", "AC Chair", "First Class", "General", "Luxury"
-        };
+        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        System.out.println("Before Sorting:");
-        System.out.println(Arrays.toString(bogieNames));
+        Scanner sc = new Scanner(System.in);
 
-        // 🔥 Built-in sort (alphabetical for String)
-        Arrays.sort(bogieNames);
+        System.out.print("Enter Bogie ID to search: ");
+        String searchKey = sc.nextLine();
 
-        System.out.println("\nAfter Sorting:");
-        System.out.println(Arrays.toString(bogieNames));
+        boolean found = linearSearch(bogieIds, searchKey);
 
-        System.out.println("\nSorting completed...");
+        if (found) {
+            System.out.println("Bogie FOUND ");
+        } else {
+            System.out.println("Bogie NOT FOUND ");
+        }
+
+        sc.close();
     }
 }
